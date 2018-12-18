765e84ebbd2a8b8449f265ba30880d903947e1a0
CodingRocker <CodingRocker@gmail.com>
2018-12-18 15:44:29 -0800

    L125

---
+++ String/L125_Valid_Palindrome.py
@@ -0,0 +1,30 @@
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left,right=0,len(s)-1
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left < right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
