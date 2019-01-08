a7c036d07dbf1b1d0a10a4fb064d1495381d756a
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-08 14:37:02 -0800

    Create L217_Contains Duplicate.py

---
+++ Array/L217_Contains Duplicate.py
@@ -0,0 +1,41 @@
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+//method 1
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+       
+        return len(set(nums))!=len(nums)
+        
+
+// method2
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                    return True
+        return False
