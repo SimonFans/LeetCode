e268a5973c9f9f1494bf28f5fbee4393819604d1
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-09 15:57:46 -0800

    Create L532_K-diff Pairs in an Array.py

---
+++ Array/L532_K-diff Pairs in an Array.py
@@ -0,0 +1,26 @@
+Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+
+Example 1:
+Input: [3, 1, 4, 1, 5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+Example 2:
+Input:[1, 2, 3, 4, 5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+
+class Solution:
+    def findPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k>0:
+            return len(set(nums) & set(k+n for n in nums))
+        # when k==0: count the # of occurence for each number in a dict{}
+        if k==0:
+            return sum(i>1 for i in collections.Counter(nums).values())
+        if k<0:
+            return 0
