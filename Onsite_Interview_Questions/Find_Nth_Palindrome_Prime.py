a55bb458da828eefd7b064089acef4a0ccd2315c
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-10 16:37:58 -0800

    Update and rename Find_Nth_Prime.py to Find_Nth_Palindrome_Prime.py

--- Onsite_Interview_Questions/Find_Nth_Palindrome_Prime.py
+++ Onsite_Interview_Questions/Find_Nth_Palindrome_Prime.py
@@ -10,14 +10,25 @@ class Solution():
                     return False
             return True
 
+    def isPalindrome(self,n):
+        t=str(n)
+        l,r=0,len(t)-1
+        while l<=r:
+            if t[l]==t[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+        return True
+
     def find_Prime(self,number):
         if number==1:
             return 2
         num=3
         count=1
-        while(True):      
-            if self.isPrime(num):  
-                count+=1     
+        while(True):
+            if self.isPrime(num) and self.isPalindrome(num):
+                count+=1
                 res=num      # save current result and return when finish
             num+=2   # avoid even number
             if count==number:   # stop when reach to the demand
@@ -26,5 +37,5 @@ class Solution():
         return res
 
 test=Solution()
-number=95
+number=6
 print(test.find_Prime(number))
