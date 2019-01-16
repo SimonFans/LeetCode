a471a952c3fa2a08ec35197152c197a8611487b4
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-15 21:43:31 -0800

    Update L349_Intersection_Two_Arrays.py

--- BinarySearch/L349_Intersection_Two_Arrays.py
+++ BinarySearch/L349_Intersection_Two_Arrays.py
@@ -38,3 +38,15 @@ class Solution:
                 j += 1
     
         return res
+    
+    
+    # Method 2 using Hash table
+    class Solution:
+        def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+            nums1 = set(nums1)
+        return [x for x in set(nums2) if x in nums1]
