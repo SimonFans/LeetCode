c15915f66d77f988f41299fbc46307c2b2c6833b
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-16 10:36:21 -0800

    Create L575_Distribute Candies.py

---
+++ Hash_Table/L575_Distribute Candies.py
@@ -0,0 +1,31 @@
+Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too. 
+The sister has three different kinds of candies. 
+Example 2:
+Input: candies = [1,1,2,3]
+Output: 2
+Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1]. 
+The sister has two different kinds of candies, the brother has only one kind of candies. 
+
+
+class Solution:
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        res=0
+        num=len(candies)
+        kind=len(set(candies))
+        if kind < num//2:
+            res=kind
+        else:
+            res=num//2
+        return res
+        
+        
