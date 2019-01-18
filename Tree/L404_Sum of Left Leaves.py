a1fa539d70108871f3d6a70d034e227553b36234
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-18 15:47:31 -0800

    Create L404_Sum of Left Leaves.py

---
+++ Tree/L404_Sum of Left Leaves.py
@@ -0,0 +1,26 @@
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        
+        
