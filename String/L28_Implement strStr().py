14476f0884b4a2bf5c00bc0d55bd3fc9b6a7b2c8
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-17 16:54:29 -0800

    Create L28_Implement strStr().py

---
+++ String/L28_Implement strStr().py
@@ -0,0 +1,57 @@
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        
+        """
+        
+        if not needle:
+            return 0
+        
+        for i in xrange(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+        
+
+思路二
+
+扫描haystack，当遇到与needle首字符相同的位置时，检查haystack从该位置开始的与needle长度相同的块，与needle是否相同。
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        for i in xrange(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                j = 1
+                while j < len(needle) and haystack[i+j] == needle[j]:
+                    j += 1
+                if j == len(needle):
+                    return i
+        return -1
