61d29008d683732bd9ac367e88d3aef27cf993a8
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-19 18:44:57 -0800

    Create L146_LRU Cache.py

---
+++ Design/L146_LRU Cache.py
@@ -0,0 +1,67 @@
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        LRUCache.capacity = capacity
+        LRUCache.length = 0
+        LRUCache.dict = collections.OrderedDict()
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        try:
+            value = LRUCache.dict[key]
+            del LRUCache.dict[key]
+            LRUCache.dict[key] = value
+            return value
+        except:
+            return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        try:
+            del LRUCache.dict[key]
+            LRUCache.dict[key] = value
+        except:
+            if LRUCache.length == LRUCache.capacity:
+                LRUCache.dict.popitem(last = False)
+                LRUCache.length -= 1
+            LRUCache.dict[key] = value
+            LRUCache.length +=1
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
