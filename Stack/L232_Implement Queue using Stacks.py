fb3f9420d6aeead4e9dd749c66b159439c15dca1
Simon Zhao <Ximengzhao1220@gmail.com>
2019-01-21 22:25:34 -0800

    Create L232_Implement Queue using Stacks.py

---
+++ Stack/L232_Implement Queue using Stacks.py
@@ -0,0 +1,60 @@
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2= []
+
+   
+    def push(self, x):
+        self.stack1.append(x)
+        
+    
+    def pop(self):
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+                
+    
+    def peek(self):
+        if self.stack2:
+            return self.stack2[-1]
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+   
+    def empty(self):
+        return not self.stack1 and not self.stack2
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
