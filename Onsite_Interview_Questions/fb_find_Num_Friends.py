e9e1ccac0072cf42a3e89e19a134786572a9e3cf
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-05 18:29:33 -0800

    Create fb_find_Num_Friends.py

---
+++ Onsite_Interview_Questions/fb_find_Num_Friends.py
@@ -0,0 +1,26 @@
+
+# find # of friends for each one
+input: [[A,B], [B,C], [A,C], [B,D], [E]]
+output: {A:2, B:3, C:2, D:1, E:0}
+
+
+
+
+class solution(object):
+    def getFriendNum(self,friend):
+        res=dict()
+        if len(friend)==0:
+            return 0
+        for item in friend:
+            if len(item)>1:
+                for susItem in item:
+                    if susItem not in res:
+                        res[susItem]=1
+                    else:
+                        res[susItem]=res.get(susItem)+1
+            else:
+                res[item[0]]=1
+        return res
+        
+        
+ 
