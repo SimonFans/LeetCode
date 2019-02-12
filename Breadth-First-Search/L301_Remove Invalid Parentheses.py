29860ca37705ffbd065e806716bcbfaf9f7144b1
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-11 17:56:04 -0800

    Create L301_Remove Invalid Parentheses.py

---
+++ Breadth-First-Search/L301_Remove Invalid Parentheses.py
@@ -0,0 +1,56 @@
+Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+Note: The input string may contain letters other than the parentheses ( and ).
+
+Example 1:
+
+Input: "()())()"
+Output: ["()()()", "(())()"]
+
+Example 2:
+
+Input: "(a)())()"
+Output: ["(a)()()", "(a())()"]
+
+Example 3:
+
+Input: ")("
+Output: [""]
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: 'str') -> 'List[str]':
+            
+        """
+        BFS solve 
+        :type s: str
+        :rtype: List[str]
+        """
+        if not s: return ['']
+        q, ans, vis = [s], [], set([s])
+        found = False
+        while q:
+            cur = q.pop(0)
+            if self.isValidParentheses(cur):
+                found = True
+                ans.append(cur)
+            elif not found:
+                for i in range(len(cur)):
+                    if cur[i] == '(' or cur[i] == ')':
+                        t = cur[:i] + cur[i + 1:]
+                        if t not in vis:
+                            q.append(t)
+                            vis.add(t)
+                found=False
+        return ans
+
+    def isValidParentheses(self, s):
+        cnt = 0
+        for c in s:
+            if c == '(':
+                cnt += 1
+            elif c == ')':
+                if cnt == 0: return False
+                cnt -= 1
+        return cnt == 0
+        
