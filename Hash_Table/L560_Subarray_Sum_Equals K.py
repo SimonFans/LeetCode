211011c9fcc34359bb2d052022837f968f281578
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-13 23:14:48 -0800

    Create L560_Subarray_Sum_Equals K.py

---
+++ Hash_Table/L560_Subarray_Sum_Equals K.py
@@ -0,0 +1,18 @@
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+
+class Solution:
+    def subarraySum(self, nums: 'List[int]', k: 'int') -> 'int':
+        ans=0  # return result
+        sum=0  # do sum 
+        d = collections.defaultdict(int)
+        d[0]=1  # initial condition
+        for i in range(len(nums)):
+            sum+=nums[i]  # 前 n sum
+            if sum-k in d:   # 前 n 项和-k 在字典里
+                ans+=d[sum-k]   # 拿出之前加到这个数，有多少个给ans
+            d[sum]+=1    # 每次把当前和记录当字典中
+        return ans
