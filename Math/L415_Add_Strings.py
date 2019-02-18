f5f3c5c8862a57771af998f1075cc4101d7f19fa
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-17 23:21:10 -0800

    Update L415_Add_Strings.py

--- Math/L415_Add_Strings.py
+++ Math/L415_Add_Strings.py
@@ -26,4 +26,52 @@ class Solution:
         return ''.join(result[::-1])
             
                 
-        
+# Follow up: 如果是小数的话
+
+class Solution:
+    def addStrings(self, num1: 'str', num2: 'str') -> 'str':
+        result=[]
+        carry=0
+
+        index1 = num1.find(".")
+        index2 = num2.find(".")
+
+        if index1 !=-1 and index2==-1:
+            result.append(num1[index1:])
+            num1=num1[:index1]
+        if index1 ==-1 and index2!=-1:
+            result.append(num2[index2:])
+            num2 = num2[:index2]
+        if index1!=-1 and index2!=-1:
+            if len(num1[index1 + 1:]) < len(num2[index2 + 1:]):
+                num1 = num1 + '0' * (len(num2[index2 + 1:]) - len(num1[index1 + 1:]))
+            else:
+                num2 = num2 + '0' * (len(num1[index1 + 1:]) - len(num2[index2 + 1:]))
+
+        l1, l2 = len(num1), len(num2)
+        while l1 or l2 or carry:
+            if num1[l1 - 1] != '.' and num2[l2 - 1] != '.':
+                digits=carry
+                if l1:
+                    l1-=1
+                    digits+=int(num1[l1])
+                if l2:
+                    l2-=1
+                    digits+=int(num2[l2])
+
+                carry=digits>9
+                result.append(str(digits%10))
+            else:
+                result.append('.')
+                l1-=1
+                l2-=1
+
+        return ''.join(result[::-1])
+
+print(Solution().addStrings('2','2'))
+print(Solution().addStrings('2','2.1'))
+print(Solution().addStrings('2.2','2'))
+print(Solution().addStrings('2.6','2.5'))
+print(Solution().addStrings('2.4','2.5'))
+
+
