1bad48cd35f22181b77311c77cf4d513b393469d
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-18 18:38:01 -0800

    Update L268_MissingNumber.py

--- Array/L268_MissingNumber.py
+++ Array/L268_MissingNumber.py
@@ -19,3 +19,18 @@ class Solution:
         
         n=len(nums)
         return n*(n+1)//2-sum(nums)
+
+    
+# Method 2:
+
+class Solution:
+    def missingNumber(self, nums: 'List[int]') -> 'int':
+        nums.sort()
+        if nums[-1]!=len(nums):
+            return len(nums)
+        if nums[0]!=0:
+            return 0
+        for i in range(1,len(nums)):
+            expected_number=nums[i-1]+1
+            if nums[i]!=expected_number:
+                return expected_number
