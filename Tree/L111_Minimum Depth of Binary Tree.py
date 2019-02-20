b56022e02b121b9919ecff02186d8dedfcb45b61
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-20 10:57:29 -0800

    Update L111_Minimum Depth of Binary Tree.py

--- Tree/L111_Minimum Depth of Binary Tree.py
+++ Tree/L111_Minimum Depth of Binary Tree.py
@@ -16,8 +16,15 @@ Given binary tree [3,9,20,null,null,15,7],
 return its minimum depth = 2.
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-    def minDepth(self, root: 'TreeNode') -> 'int':
+    def maxDepth(self, root: 'TreeNode') -> 'int':
         if root is None: 
             return 0 
       
@@ -27,12 +34,11 @@ class Solution:
       
         # If left subtree is Null, recur for right subtree 
         if root.left is None: 
-            return self.minDepth(root.right)+1
+            return self.maxDepth(root.right)+1
       
         # If right subtree is Null , recur for left subtree 
         if root.right is None: 
-            return self.minDepth(root.left) +1 
+            return self.maxDepth(root.left) +1 
       
-        return min(self.minDepth(root.left), self.minDepth(root.right))+1
-        
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
  
