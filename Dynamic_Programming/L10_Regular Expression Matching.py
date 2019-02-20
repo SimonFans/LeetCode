f7fba0f2227845260eb48a49f12c8200d9277c05
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-20 00:45:37 -0800

    Update L10_Regular Expression Matching.py

--- Dynamic_Programming/L10_Regular Expression Matching.py
+++ Dynamic_Programming/L10_Regular Expression Matching.py
@@ -62,12 +62,17 @@ class Solution:
                     dp[0][i]=dp[0][i-2]
         for i in range(1,len(s)+1):
             for j in range(1,len(p)+1):
-                if p[j-1]=='.':
+                if p[j-1]=='.' or p[j-1]==s[i-1]:
                     dp[i][j]=dp[i-1][j-1]
+                
+                # abcdd   abcd* one or more match
                 elif p[j-1]=='*':
-                    dp[i][j]=dp[i][j-1] or dp[i][j-2] or (dp[i-1][j] and (s[i-1]==p[j-2] or p[j-2]=='.'))
-                else:
-                    dp[i][j]=dp[i-1][j-1] and s[i-1]==p[j-1]
+                    if p[j-2]==s[i-1] or p[j-2]=='.':
+                        dp[i][j]=dp[i][j-2] or dp[i-1][j]
+                
+                # abcd  abcde*  match 0 case
+                    else:
+                        dp[i][j]=dp[i][j-2] 
         return dp[len(s)][len(p)]
   
 # if only match '.'  => see below solution
