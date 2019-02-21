e47a17c6b19647815e62214d725543141311b6ce
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-20 20:55:05 -0800

    Update L678_Valid Parenthesis String.py

--- String/L678_Valid Parenthesis String.py
+++ String/L678_Valid Parenthesis String.py
@@ -30,7 +30,7 @@ class Solution:
                 hi-=1
             if hi < 0: break
             lo = max(lo, 0)
-            
+        return lo == 0      
 Time Complexity: O(N)O(N), where NN is the length of the string. We iterate through the string once.
 
 Space Complexity: O(1)O(1), the space used by our lo and hi pointers. However, creating a new character array will take O(N)O(N) space.
