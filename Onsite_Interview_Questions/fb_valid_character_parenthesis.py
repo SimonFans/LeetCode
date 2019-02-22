7ef97ca1a6faa0014513f94ef2cd9250f6ffaca1
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-22 09:30:32 -0800

    Update fb_valid_character_parenthesis.py

--- Onsite_Interview_Questions/fb_valid_character_parenthesis.py
+++ Onsite_Interview_Questions/fb_valid_character_parenthesis.py
@@ -6,19 +6,22 @@ def valid_String(str):
     if len(str)==0:
         return False
     for i in str:
+        # 如果是非括号元素就继续
         if i.isalpha():
             continue
+        # 如果stack为空，且遇到右侧括号，返回False
         if i in Map.keys() and len(stack)==0:
             return False
-
+        # 如果遇到左侧括号，直接加入stack
         if i in ['(','{','[','<']:
             stack.append(i)
+        # 否则当stack不为空时，取出stack最后一个，和当前的做比较，不匹配，返回False
         else:
             if len(stack)!=0:
                 cur=stack.pop()
             if Map.get(i)!=cur:
                 return False
-
+    # 最后看stack是不是为空
     return len(stack)==0
 
 
