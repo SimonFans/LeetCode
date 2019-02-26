0065ce8cb6b4daa4989ac06d22298147c28d8150
Simon Zhao <Ximengzhao1220@gmail.com>
2019-02-25 22:00:30 -0800

    Create L102_Binary Tree Level Order Traversal.py

---
+++ Breadth-First-Search/L102_Binary Tree Level Order Traversal.py
@@ -0,0 +1,42 @@
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        queue = collections.deque()
+        res=[]
+        if root==None:
+            return res
+        queue.append(root)
+        
+        while queue:
+            level=[]
+            for i in range(len(queue)):
+                node=queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
