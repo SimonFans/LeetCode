11d4d69fffca6b81e9b1a2d021a989aa9fa12657
Simon Zhao <Ximengzhao1220@gmail.com>
2019-03-04 18:08:18 -0800

    Update L169_Majority Element.py

--- Array/L169_Majority Element.py
+++ Array/L169_Majority Element.py
@@ -11,24 +11,18 @@ Example 2:
 Input: [2,2,1,1,1,2,2]
 Output: 2
 
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # [8,8,7,7,7]
-        
-        res=nums[0]
-        count=1
-        for i in range(1,len(nums)):
-            if count==0:
-                res,count=nums[i],1
-            else:
-                if res==nums[i]:
-                    count+=1
-                else:
-                    count-=1
-        return res
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maj_index = 0
+        count = 1
+        for i in range(1,len(nums)): 
+            if nums[maj_index] == nums[i]: 
+                count += 1
+            else: 
+                count -= 1
+            if count == 0: 
+                maj_index = i 
+                count = 1
+        return nums[maj_index] 
         
      
