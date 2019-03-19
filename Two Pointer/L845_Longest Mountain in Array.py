d111deae4d6147b805d90035b20339fb1e117785
Simon Zhao <Ximengzhao1220@gmail.com>
2019-03-19 13:27:20 -0700

    Create L845_Longest Mountain in Array.py

---
+++ Two Pointer/L845_Longest Mountain in Array.py
@@ -0,0 +1,42 @@
+Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
+
+B.length >= 3
+There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+(Note that B could be any subarray of A, including the entire array A.)
+
+Given an array A of integers, return the length of the longest mountain. 
+
+Return 0 if there is no mountain.
+
+Example 1:
+
+Input: [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+Example 2:
+
+Input: [2,2,2]
+Output: 0
+Explanation: There is no mountain.
+Note:
+
+0 <= A.length <= 10000
+0 <= A[i] <= 10000
+
+class Solution:
+    def longestMountain(self, A: List[int]) -> int:
+        N=len(A)
+        ans=base=0
+        while base <N:
+            end=base
+            if end+1 < N and A[end]<A[end+1]:
+                while end+1 < N and A[end]<A[end+1]:
+                    end+=1
+                if end+1 < N and A[end]>A[end+1]:
+                    while end+1 < N and A[end]>A[end+1]:
+                        end+=1
+                    ans=max(ans,end-base+1) # [2,3] 没有下坡就不算结果
+            base=max(end,base+1)   # in case [2,1,4,7,3,2,5] 没有上坡的情况，更新Base
+        return ans
+        
+        
