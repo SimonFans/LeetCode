6cd9de8ed0b91d45b5c26b41a555a8f5561ffc01
Simon Zhao <Ximengzhao1220@gmail.com>
2019-03-22 15:42:15 -0700

    Update L114_Flatten Binary Tree to Linked List.py

--- Tree/L114_Flatten Binary Tree to Linked List.py
+++ Tree/L114_Flatten Binary Tree to Linked List.py
@@ -20,13 +20,16 @@ The flattened tree should look like:
         5
          \
           6
-          
+
+思路： 
+stack先压右侧节点，再左侧，之后将新建的node节点右侧指针链接上个节点的左侧，所以要用stack存储
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
 
 class Solution:
     def flatten(self, root: TreeNode) -> None:
