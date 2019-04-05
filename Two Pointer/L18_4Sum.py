7e08dce8ad81ba7d2d899b2af900590246a46a9c
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-04 22:38:33 -0700

    Update L18_4Sum.py

--- Two Pointer/L18_4Sum.py
+++ Two Pointer/L18_4Sum.py
@@ -17,33 +17,28 @@ A solution set is:
 
 
 class Solution:
-    def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        d = dict()
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if len(nums)<4:
+            return []
+        dic=dict()
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                sum2 = nums[i]+nums[j]
-                if sum2 in d:
-                    d[sum2].append((i,j))
+                sum2=nums[i]+nums[j]
+                if sum2 in dic:
+                    dic[sum2].append((i,j))
                 else:
-                    d[sum2] = [(i,j)]
-        
-        result = set()
-        for key in d:
-            value = target - key
-            if value in d:
-                list1 = d[key]
-                list2 = d[value]
-                for (i,j) in list1:
-                    for (k,l) in list2:
-                        if i!=k and i!=l and j!=k and j!=l:
-                            flist = [nums[i],nums[j],nums[k],nums[l]]
-                            flist.sort()
-                            result.add(tuple(flist))
-        return list(result)
-        
+                    dic[sum2]=[(i,j)]
+        res=set()
+        for key in dic:
+            value=target-key
+            if value in dic:
+                list1=dic[key]
+                list2=dic[value]
+                for (a,b) in list1:
+                    for (c,d) in list2:
+                        if a!=c and a!=d and b!=c and b!=d:
+                            temp=[nums[a],nums[b],nums[c],nums[d]]
+                            temp.sort()
+                            res.add(tuple(temp))
+        return list(res)
         
