d20d139f67873d7bb92a9f8c6e63d8d4d2df755e
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-10 16:59:28 -0700

    Update L81_Search_In_Rotated_Sorted_ArrayII.py

--- BinarySearch/L81_Search_In_Rotated_Sorted_ArrayII.py
+++ BinarySearch/L81_Search_In_Rotated_Sorted_ArrayII.py
@@ -26,7 +26,7 @@ class Solution:
         """
         left=0; right=len(nums)-1
         while left<=right:
-            mid=(left+right)//2
+            mid=left+(right-left)//2
             if nums[mid]==target: return True
             if nums[left]==nums[mid]==nums[right]: # for duplicated number
                 left+=1; right-=1
