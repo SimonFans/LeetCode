799173d3894d70e36e49e49a9c40645d997c0a1a
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-15 21:45:34 -0700

    Update L105_Construct Binary Tree from Preorder and Inorder Traversal.py

--- Tree/L105_Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Tree/L105_Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -15,7 +15,6 @@ Return the following binary tree:
     /  \
    15   7
    
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -25,7 +24,7 @@ Return the following binary tree:
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        if preorder is None or inorder is None or len(preorder)==0 or len(preorder)!=len(inorder):
+        if len(preorder)==0 or len(preorder)!=len(inorder):
             return None
         return self.buildTreeHelper(preorder,inorder,0,0,len(preorder)-1)
         
@@ -33,11 +32,8 @@ class Solution:
         if pre_st>len(preorder) or ino_st>ino_end:
             return None
         current=TreeNode(preorder[pre_st])
-        i=ino_st
-        while i<=ino_end:
-            if inorder[i]==preorder[pre_st]:
-                break
-            i+=1
+        i=inorder.index(preorder[pre_st])
+       
         current.left=self.buildTreeHelper(preorder,inorder,pre_st+1,ino_st,i-1)
         current.right=self.buildTreeHelper(preorder,inorder,pre_st+(i-ino_st+1),i+1,ino_end)
         return current
