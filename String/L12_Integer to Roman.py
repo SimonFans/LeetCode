dc11c4fe0edf1e2a571258b53e1da666e35f318a
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-20 17:47:41 -0700

    Update L12_Integer to Roman.py

--- String/L12_Integer to Roman.py
+++ String/L12_Integer to Roman.py
@@ -42,16 +42,12 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 
 class Solution:
-    def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
+    def intToRoman(self, num: int) -> str:
         values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
         numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
-        list = ''
-        for i in range(0, len(values)):
-            while num >= values[i]:
-                num -= values[i]
-                list += numerals[i]
-        return list
+        res=''
+        for i in range(len(values)):
+            while num>=values[i]:
+                num-=values[i]
+                res+=numerals[i]
+        return res
