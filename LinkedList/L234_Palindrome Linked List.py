8203537dd6044e08ffdb45dc3b7a618c6f581994
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-22 16:20:47 -0700

    Update L234_Palindrome Linked List.py

--- LinkedList/L234_Palindrome Linked List.py
+++ LinkedList/L234_Palindrome Linked List.py
@@ -14,34 +14,42 @@ Could you do it in O(n) time and O(1) space?
 # Time complexity O(n)
 # reverseList() 方法只是反转链表的一种方法，空间复杂度 O(1). 不借助多余的空间实现反转单链表后半部分，则可以实现空间复杂度 O(1) 的要求
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
+    def isPalindrome(self, head: ListNode) -> bool:
+        
         if not head or not head.next:
             return True
-        # find the center point of the list
+        
         slow=fast=head
-        while fast.next and fast.next.next:
+        while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-        slow=slow.next
-        slow=self.reverseList(slow)
+        
+        # odd length only, even length no need, because slow is in the right position
+        if fast:
+            slow=slow.next
+        
+        slow=self.reverse_list(slow)
         
         while slow:
             if head.val!=slow.val:
                 return False
+            head=head.next
             slow=slow.next
-            head=head.next
-        return True
         
-    def reverseList(self,head):
-        newHead=None
-        while head:
-            p=head
-            head=head.next
-            p.next=newHead
-            newHead=p
-        return newHead
+        return True
+    
+    def reverse_list(self, slow):
+        pre=None
+        while slow:
+            next_node=slow.next
+            slow.next=pre
+            pre=slow
+            slow=next_node
+        return pre
