99c3fbb1e951b878e1c03334a81b0837d50d42e7
Simon Zhao <Ximengzhao1220@gmail.com>
2019-04-22 17:08:07 -0700

    Update L21_Merge Two Sorted Lists.py

--- LinkedList/L21_Merge Two Sorted Lists.py
+++ LinkedList/L21_Merge Two Sorted Lists.py
@@ -8,39 +8,26 @@ Output: 1->1->2->3->4->4
 
 
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        dummy=ListNode(0)
-        cur=dummy
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        cur=ListNode(float('-Inf'))
+        dummy=cur
+        
         while l1 and l2:
             if l1.val<l2.val:
-                cur.next=ListNode(l1.val)
+                cur.next=l1
                 l1=l1.next
-                cur=cur.next
             else:
-                cur.next=ListNode(l2.val)
+                cur.next=l2
                 l2=l2.next
-                cur=cur.next
-        while l1:
-            cur.next=ListNode(l1.val)
-            l1=l1.next
             cur=cur.next
-        while l2:
-            cur.next=ListNode(l2.val)
-            l2=l2.next
-            cur=cur.next
+        
+        cur.next = l1 or l2
+        
         return dummy.next
