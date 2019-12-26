4de30073dc948e2d1534f38489fb2f5b379ae0f7
Simon Zhao <Ximengzhao1220@gmail.com>
2019-12-26 11:28:22 -0800

    Update L138_CopyListWithRandomPointer.py

--- LinkedList/L138_CopyListWithRandomPointer.py
+++ LinkedList/L138_CopyListWithRandomPointer.py
@@ -2,16 +2,6 @@ A linked list is given such that each node contains an additional random pointer
 
 Return a deep copy of the list.
 
-
-# Definition for singly-linked list with a random pointer.
-# class RandomListNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.next = None
-#         self.random = None
-#   1-2-3-4  1-3, 4-2
-# 先复制，字典存储，之后while走random
-
 """
 https://www.cnblogs.com/zuoyuan/p/3745126.html
 
@@ -22,27 +12,38 @@ https://www.cnblogs.com/zuoyuan/p/3745126.html
 第三步，将新的链表从上图这样的链表中拆分出来。
 """
 
-class Solution(object):
-    def copyRandomList(self, head):
-        """
-        :type head: RandomListNode
-        :rtype: RandomListNode
-        """
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+# example: 1-1'-2-2'-3-3'-4-4', random pointer: 1-3, 2-4
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
         if head == None: return None
         tmp = head
+        # (1) duplicate a new node such as: 1-1'-2-2'-3-3'-4-4'
         while tmp:
-            newNode = RandomListNode(tmp.label)
+            newNode = Node(tmp.val)
             newNode.next = tmp.next
             tmp.next = newNode
             tmp = tmp.next.next
         tmp = head
+        # (2) Copy each node random pointer
         while tmp:
             if tmp.random:
                 tmp.next.random = tmp.random.next
             tmp = tmp.next.next
+        # reset old head and new head (copied one)
         newhead = head.next
         pold = head
         pnew = newhead
+        # separate copied one from original one
         while pnew.next:
             pold.next = pnew.next
             pold = pold.next
@@ -50,6 +51,7 @@ class Solution(object):
             pnew = pnew.next
         pold.next = None
         pnew.next = None
+        # Return copied one: 1'-2'-3'-4'
         return newhead
     
     
