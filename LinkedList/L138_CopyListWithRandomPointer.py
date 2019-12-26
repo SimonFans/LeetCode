2f792b7f20fc70fef1ce46fe694bf81d6f29c1f0
Simon Zhao <Ximengzhao1220@gmail.com>
2019-12-26 11:17:16 -0800

    Update L138_CopyListWithRandomPointer.py

--- LinkedList/L138_CopyListWithRandomPointer.py
+++ LinkedList/L138_CopyListWithRandomPointer.py
@@ -12,25 +12,44 @@ Return a deep copy of the list.
 #   1-2-3-4  1-3, 4-2
 # 先复制，字典存储，之后while走random
 
+"""
+https://www.cnblogs.com/zuoyuan/p/3745126.html
+
+首先，在原链表的每个节点后面都插入一个新节点，新节点的内容和前面的节点一样。比如上图，1后面插入1，2后面插入2，依次类推。
+
+其次，原链表中的random指针如何映射呢？比如上图中，1节点的random指针指向3，4节点的random指针指向2。如果有一个tmp指针指向1（蓝色），则一条语句：tmp.next.random = tmp.random.next；就可以解决这个问题。
+
+第三步，将新的链表从上图这样的链表中拆分出来。
+"""
+
 class Solution(object):
     def copyRandomList(self, head):
         """
         :type head: RandomListNode
         :rtype: RandomListNode
         """
-        if head is None:
-            return None
-        pointer,dummy=head,RandomListNode(0)
-        newHead=dummy
-        nodeDic=dict()
-        while pointer:
-            newNode=RandomListNode(pointer.label)
-            nodeDic[pointer]=newHead.next=newNode
-            pointer,newHead=pointer.next,newHead.next
-        pointer=head
-        newHead=dummy.next
-        while newHead:
-            if pointer.random:
-                newHead.random=nodeDic[pointer.random]
-            pointer,newHead=pointer.next,newHead.next
-        return dummy.next
+        if head == None: return None
+        tmp = head
+        while tmp:
+            newNode = RandomListNode(tmp.label)
+            newNode.next = tmp.next
+            tmp.next = newNode
+            tmp = tmp.next.next
+        tmp = head
+        while tmp:
+            if tmp.random:
+                tmp.next.random = tmp.random.next
+            tmp = tmp.next.next
+        newhead = head.next
+        pold = head
+        pnew = newhead
+        while pnew.next:
+            pold.next = pnew.next
+            pold = pold.next
+            pnew.next = pold.next
+            pnew = pnew.next
+        pold.next = None
+        pnew.next = None
+        return newhead
+    
+    
