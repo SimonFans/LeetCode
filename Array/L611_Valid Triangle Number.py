4563f6756d8c920417948485533e1930d9bafd38
Simon Zhao <Ximengzhao1220@gmail.com>
2019-12-30 10:28:31 -0800

    Update L611_Valid Triangle Number.py

--- Array/L611_Valid Triangle Number.py
+++ Array/L611_Valid Triangle Number.py
@@ -14,18 +14,32 @@ Note:
 The length of the given array won't exceed 1000.
 The integers in the given array are in the range of [0, 1000].
 
+"""
+思路：
+
+9 8 7 6 5 4 3 2 
+c b           a
+
+(1) 先做降序排列
+(2) while b<a: 
+        if c>a+b
+            res+=(a-b)  // 如果cba可以，证明c 和b a 之间的都可以
+            b+=1
+        else:
+            a-=1
+
+"""
+
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
         res=0
-        nums.sort()  # sort the list, small to large
-        # From the largest number, use left, right pointer
-        for i in range(len(nums)-1,1,-1):
-            l=0
-            r=i-1
-            while l<r:
-                if nums[l]+nums[r]>nums[i]:
-                    res+=r-l
-                    r-=1
+        for c in range(len(nums)-2):
+            b,a=c+1,len(nums)-1
+            while a>b:
+                if nums[a]+nums[b]>nums[c]:
+                    res+=a-b
+                    b+=1
                 else:
-                    l+=1
+                    a-=1
         return res
