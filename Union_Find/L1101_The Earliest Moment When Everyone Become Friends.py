f3232f01c6c8e207966c7300bdf4b2fd58209099
Simon Zhao <Ximengzhao1220@gmail.com>
2019-12-30 17:52:22 -0800

    Create L1101_The Earliest Moment When Everyone Become Friends.py

---
+++ Union_Find/L1101_The Earliest Moment When Everyone Become Friends.py
@@ -0,0 +1,95 @@
+The Earliest Moment When Everyone Become Friends
+In a social group, there are N people, with unique integer ids from 0 to N-1.
+
+We have a list of logs, where each logs[i] = [timestamp, id_A, id_B] contains a non-negative integer timestamp, and the ids of two different people.
+
+Each log represents the time in which two different people became friends.  Friendship is symmetric: if A is friends with B, then B is friends with A.
+
+Let's say that person A is acquainted with person B if A is friends with B, or A is a friend of someone acquainted with B.
+
+Return the earliest time for which every person became acquainted with every other person. Return -1 if there is no such earliest time.
+
+ 
+
+Example 1:
+
+Input: logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]], N = 6
+Output: 20190301
+Explanation: 
+The first event occurs at timestamp = 20190101 and after 0 and 1 become friends we have the following friendship groups [0,1], [2], [3], [4], [5].
+The second event occurs at timestamp = 20190104 and after 3 and 4 become friends we have the following friendship groups [0,1], [2], [3,4], [5].
+The third event occurs at timestamp = 20190107 and after 2 and 3 become friends we have the following friendship groups [0,1], [2,3,4], [5].
+The fourth event occurs at timestamp = 20190211 and after 1 and 5 become friends we have the following friendship groups [0,1,5], [2,3,4].
+The fifth event occurs at timestamp = 20190224 and as 2 and 4 are already friend anything happens.
+The sixth event occurs at timestamp = 20190301 and after 0 and 3 become friends we have that all become friends.
+ 
+
+Note:
+
+2 <= N <= 100
+1 <= logs.length <= 10^4
+0 <= logs[i][0] <= 10^9
+0 <= logs[i][1], logs[i][2] <= N - 1
+It's guaranteed that all timestamps in logs[i][0] are different.
+logs are not necessarily ordered by some criteria.
+logs[i][1] != logs[i][2]
+
+
+
+class UnionFindSet(object):
+    def __init__(self, n):
+        self.roots = [i for i in range(n)] # [0 1 2 3 4 5]
+        self.rank = [0 for i in range(n)]  # [0 0 0 0 0 0]
+        self.count = 0
+    
+    def find(self, member):
+        tmp = []
+        while member != self.roots[member]:
+            tmp.append(member)
+            member = self.roots[member]
+        for root in tmp:
+            self.roots[root] = member
+        return member
+        
+    def union(self, p, q):
+        parentP = self.find(p)
+        parentQ = self.find(q)
+        if parentP != parentQ:
+            if self.rank[parentP] > self.rank[parentQ]:
+                self.roots[parentQ] = parentP
+            elif self.rank[parentP] < self.rank[parentQ]:
+                self.roots[parentP] = parentQ
+            else:
+                self.roots[parentQ] = parentP
+                self.rank[parentP] -= 1
+            self.count -= 1
+    
+    def check(self):
+        # print self.roots
+        flag = self.find(self.roots[0])
+        for i in self.roots:
+            if self.find(i) != flag:
+                return False
+            print(self.roots)
+            print(self.rank)
+        return True
+
+class Solution(object):
+    def earliestAcq(self, logs, N):
+        """
+        :type logs: List[List[int]]
+        :type N: int
+        :rtype: int
+        """
+        logs = sorted(logs, key = lambda x:x[0])
+        ufs = UnionFindSet(N)
+        for time, x, y in logs:
+            ufs.union(x, y)
+            if ufs.check():
+                return time
+        return -1
+
+    
+logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]]
+print(Solution().earliestAcq(logs,6))
+
