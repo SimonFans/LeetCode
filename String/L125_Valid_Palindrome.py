ed2aef8c4c328ce3ae65d98d68c621e0e1ae9ea9
Simon Zhao <Ximengzhao1220@gmail.com>
2020-01-02 12:02:02 -0800

    Update L125_Valid_Palindrome.py

--- String/L125_Valid_Palindrome.py
+++ String/L125_Valid_Palindrome.py
@@ -11,26 +11,9 @@ Example 2:
 Input: "race a car"
 Output: false
 
-class Solution:
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        left,right=0,len(s)-1
-        while left<right:
-            while left<right and not s[left].isalnum():
-                left+=1
-            while left < right and not s[right].isalnum():
-                right-=1
-            if s[left].lower()!=s[right].lower():
-                return False
-            left+=1
-            right-=1
-        return True
 
-    
-    class Solution:
+
+class Solution:
     def isPalindrome(self, s: 'str') -> 'bool':
         left,right=0,len(s)-1
         while left < right:
