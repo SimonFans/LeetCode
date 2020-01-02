a898d3e663989b33645981a97df1a860010de80f
Simon Zhao <Ximengzhao1220@gmail.com>
2020-01-02 13:54:52 -0800

    Update FirstMissingPositive.py

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -5,24 +5,33 @@ For example,
 Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
 
+Test cases:
+
 [0,1, 2, 3]
 [3,4,-1,1]
 [1,-1,3,4]
-
 """
 
-class solution:
-    def First_Missing_Positive(self, A):
-        length = len(A)
-        for i in range(length):
-            while A[i] > 0 and A[i] < length and A[A[i]-1] != A[i]:
-                temp=A[A[i]-1]
-                A[A[i] - 1]=A[i]
-                A[i]=temp
-        for j in range(length):
-            if A[j] != j+1:
+"""
+while:
+1. nums[i]>0 => only consider positive number
+2. nums[i]< len(nums) => if larger than len(nums), value cannot fall into the list
+3. nums[nums[i]-1]!=nums[i] => 判断是不是当前这个值的index对应的是这个值，不是则交换
+return n+1: when nums=[] <empty>
+"""
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n=len(nums)
+        for i in range(n):
+            while nums[i]>0 and nums[i]< n and nums[nums[i]-1]!=nums[i]:
+                temp=nums[nums[i]-1]
+                nums[nums[i]-1]=nums[i]
+                nums[i]=temp
+        for j in range(n):
+            if nums[j]!=j+1:
                 return j+1
-        return length+1
+        return n+1
 
 A=[3,4,-1,1]
 test=solution()
