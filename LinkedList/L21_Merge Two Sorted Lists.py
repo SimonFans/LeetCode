14fa0dd7f0cf05ded3a1a8ba0de64e8081fac353
Simon Zhao <Ximengzhao1220@gmail.com>
2020-01-12 10:51:52 -0800

    Update L21_Merge Two Sorted Lists.py

--- LinkedList/L21_Merge Two Sorted Lists.py
+++ LinkedList/L21_Merge Two Sorted Lists.py
@@ -6,7 +6,7 @@ Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 
 
-
+# Method 1
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -31,3 +31,36 @@ class Solution:
         cur.next = l1 or l2
         
         return dummy.next
+
+
+# Method 2 heap sort    
+from heapq import *
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        cur=head=ListNode(0)
+        heap=[]
+        # 增加一个元素在heap里，以防止当值相同的时候对比大小
+        count=1
+        
+        if not l1 and not l2:
+            return None
+        
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        heapq.heappush(heap,(l1.val,count,l1))
+        count+=1
+        heapq.heappush(heap,(l2.val,count,l2))
+        
+        while len(heap)>0:
+            _,_,cur.next=heapq.heappop(heap)
+            cur=cur.next
+            if cur.next is not None:
+                count+=1
+                heapq.heappush(heap,(cur.next.val,count,cur.next))
+    
+        return head.next
