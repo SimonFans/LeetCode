b28f2fcbf04f4ff1d95904a0d2431b7c295f87dc
Simon Zhao <Ximengzhao1220@gmail.com>
2020-01-17 10:20:13 -0800

    Update fb_L253_minMeetingRoomsII.py

--- Onsite_Interview_Questions/fb_L253_minMeetingRoomsII.py
+++ Onsite_Interview_Questions/fb_L253_minMeetingRoomsII.py
@@ -9,25 +9,30 @@ return 2.
 
 from heapq import heappush, heappop
 
-class solution:
-    def minMeetingRoomsII(self,intervals):
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        # [[0,30],[5,10],[15,20]]
-        starts_sort=sorted(intervals, key=lambda x: x[0])   
+        if not intervals:
+            return 0
         
-        # heap中只存放结束时间，如果结束时间比当前时间早，我们就延长为当前的结束时间，否则压入房间当前结束时间
-        heap=[]
+        # 按开始时间排序
+        intervals.sort(key=lambda x:x[0])
         
-        for i in range(len(starts_sort)):
-            while heap and heap[0]<starts_sort[i][1]:
-                heappop(heap)
-                
-            heappush(heap,starts_sort[i][1])
+        # 建立堆结构
+        room=[]
+        print(intervals[0][1])
+        # 放入第一个时间
+        heappush(room,intervals[0][1])
         
-        return len(heap)
-    
-intervals=[[0,30],[5,10],[15,20]]
-print(solution().minMeetingRoomsII(intervals))
+        for i in intervals[1:]:
+            # 如果之前会议最早的结束时间<当前会议的开始时间，就将当前会议结束时间更新为最新结束时间
+            if room[0]<=i[0]:
+                heappop(room)
+            # （1）最近的会议都在开，只能开新房间 （2）找到并延长之前的会议结束时间
+            heappush(room,i[1])
+        
+        # 最后算有多少个结束时间就是多少个房间
+        return len(room)
 
 # Method 2:
 
