ceb80b7df85cabd83f3ef8fdfb6fb36a703b125a
Simon Zhao <Ximengzhao1220@gmail.com>
2020-02-17 21:50:36 -0800

    Update L10_Regular Expression Matching.py

--- Dynamic_Programming/L10_Regular Expression Matching.py
+++ Dynamic_Programming/L10_Regular Expression Matching.py
@@ -102,3 +102,30 @@ sol.isMatch('xaabyc', 'xa*b.c')
                     dp[i][j]=dp[i-1][j-1] and s[i-1]==p[j-1]
         return dp[len(s)][len(p)]
 
+
+    # another solution:
+    
+    class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p):
+                    ans = i == len(s)
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j+1 < len(p) and p[j+1] == '*':
+                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    else:
+                        ans = first_match and dp(i+1, j+1)
+
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
+    
