7732a42fbc2fe57ac880999bc68ce1578234fef1
Simon Zhao <Ximengzhao1220@gmail.com>
2020-02-25 14:24:01 -0800

    Update L35_Search Insert Position.py

--- Array/L35_Search Insert Position.py
+++ Array/L35_Search Insert Position.py
@@ -20,22 +20,23 @@ Input: [1,3,5,6], 0
 Output: 0
 
 class Solution:
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # Sorted Array 想二分法 time complexity: logn, space complexity: O(1)
-        # Binary search runs in at worst logarithmic time, making O(log n) comparisons, where n is the number of elements in the array, the O is Big O notation, and log is the logarithm. Binary search takes constant (O(1)) space, meaning that the space taken by the algorithm is the same for any number of elements in the array.
-        l=0
-        r=len(nums)-1
-        while l<=r:
-            mid=l+(r-l)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]<target:
-                l=mid+1
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if len(nums)==0:
+            return -1
+        
+        start,end=0,len(nums)-1
+        
+        # ensure start & end next to each other at the end
+        while start+1<end:
+            mid=start+(end-start)//2
+            if nums[mid]>=target:
+                end=mid
             else:
-                r=mid-1
-        return l
+                start=mid
+        if nums[start]>=target:
+            return start
+        if nums[end]>=target:
+            return end 
+        return len(nums)
+    
+    
