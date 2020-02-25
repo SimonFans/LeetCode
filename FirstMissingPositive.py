227a3b550e11d3bb11f83b8dc7cdb078d177f286
Simon Zhao <Ximengzhao1220@gmail.com>
2020-02-24 17:00:11 -0800

    Update FirstMissingPositive.py

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -12,28 +12,37 @@ Test cases:
 [1,-1,3,4]
 """
 
-"""
-while:
-1. nums[i]>0 => only consider positive number
-2. nums[i]< len(nums) => if larger than len(nums), value cannot fall into the list
-3. nums[nums[i]-1]!=nums[i] => 判断是不是当前这个值的index对应的是这个值，不是则交换
-return n+1: when nums=[] <empty>
-"""
-
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n=len(nums)
-        for i in range(n):
-            while nums[i]>0 and nums[i]<=n and nums[nums[i]-1]!=nums[i]:
-                temp=nums[nums[i]-1]
-                nums[nums[i]-1]=nums[i]
-                nums[i]=temp
-        for j in range(n):
-            if nums[j]!=j+1:
-                return j+1
-        # if [1,2,3], return 4
-        return n+1
+        if not nums:
+            return 1
 
+        length = len(nums)
+        i = 0
+        while i < length:
+            if nums[i] <= 0:
+                i += 1
+            else:
+                if nums[i] <= length:
+                    if nums[i] == i+1 or nums[nums[i]-1] == nums[i]:
+                        # no need to swap
+                        i += 1
+                        continue
+                    tmp = nums[nums[i]-1]
+                    nums[nums[i]-1] = nums[i]
+                    nums[i] = tmp
+                else:
+                    i += 1
+
+        i = 0
+        while i < length:
+            if nums[i] != i + 1:
+                return i + 1
+            i += 1
+
+        return length + 1  # no missing until length
+    
+    
 A=[3,4,-1,1]
 test=solution()
 print(test.First_Missing_Positive(A))
