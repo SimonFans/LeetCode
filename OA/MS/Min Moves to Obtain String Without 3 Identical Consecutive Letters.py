a9444d9bcfd6c19f8d76719624d043985d8a3ef5
Simon Zhao <Ximengzhao1220@gmail.com>
2020-03-15 09:48:37 -0700

    Update Min Moves to Obtain String Without 3 Identical Consecutive Letters.py

--- OA/MS/Min Moves to Obtain String Without 3 Identical Consecutive Letters.py
+++ OA/MS/Min Moves to Obtain String Without 3 Identical Consecutive Letters.py
@@ -1,17 +1,9 @@
-def findMinMoves(s):
-  if len(s) < 3:
-    return 0
-  s = [elem for elem in s]
-  moves = 0
-  index = 2
-  while index < len(s):
-    tempVal = s[index]
-    if tempVal == s[index - 1] and tempVal == s[index - 2]:
-        if (index + 2) < len(s) and tempVal == s[index + 1] and tempVal == s[index + 2]:
-            s[index] = 'a' if tempVal == 'b' else 'b'
-        else:
-            s[index - 1] = 'a' if tempVal == 'b' else 'b'
-        moves += 1
-    index += 1
+from itertools import groupby
+def solution(A):
 
-return moves
+    splitted = [''.join(g) for _, g in groupby(A)]
+    moves = 0
+    for segment in splitted:
+        moves += int(len(segment) / 3)
+
+    return moves
