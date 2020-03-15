f85445c9a51a5601b7e4ff932a7eb4848ea779f4
Simon Zhao <Ximengzhao1220@gmail.com>
2020-03-15 12:02:05 -0700

    Create Max Inserts to obtain string without 3 consecutive a.py

---
+++ OA/MS/Max Inserts to obtain string without 3 consecutive a.py
@@ -0,0 +1,18 @@
+def maxInserts(word):
+    word = "b"+word+"b"
+    idx = [i for i in range(len(word)) if word[i] != "a"]
+    count = 0
+    for i in range(1,len(idx)):
+        diff = idx[i]-idx[i-1]-1
+        if diff >= 3:
+            return -1
+        count+=2-diff
+    return count
+a = "aabab"
+print maxInserts(a)
+a = "dog"
+print maxInserts(a)
+a = "aa"
+print maxInserts(a)
+a = "baaaa"
+print maxInserts(a)
