6f6c882be9992b32013b1e289cc8941fff1c5081
Simon Zhao <Ximengzhao1220@gmail.com>
2020-03-20 12:23:00 -0700

    Update L21_Merge Two Sorted Lists.py

--- LinkedList/L21_Merge Two Sorted Lists.py
+++ LinkedList/L21_Merge Two Sorted Lists.py
@@ -32,7 +32,19 @@ class Solution:
         
         return dummy.next
 
+Time complexity : 
+O(n+m)
 
+Because exactly one of l1 and l2 is incremented on each loop iteration, 
+the while loop runs for a number of iterations equal to the sum of the lengths of the two lists. 
+All other work is constant, so the overall complexity is linear.
+
+Space complexity : 
+O(1)
+
+The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+    
+    
 # Method 2 heap sort    
 from heapq import *
 
