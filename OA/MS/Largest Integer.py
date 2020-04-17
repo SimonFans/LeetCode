0075b51aa625da00d71ed43de8879074295d0fcc
Simon Zhao <Ximengzhao1220@gmail.com>
2020-04-17 12:57:04 -0700

    Update Largest Integer.py

--- OA/MS/Largest Integer.py
+++ OA/MS/Largest Integer.py
@@ -1,6 +1,19 @@
-from collections import defaultdict
+Write a function that, given an array A of N integers, 
+returns the lagest integer K > 0 such that both values K and -K exist in array A. 
+If there is no such integer, the function should return 0.
 
-mp=defaultdict(list)
+Example 1:
+
+Input: [3, 2, -2, 5, -3]
+Output: 3
+
+Example 2:
+
+Input: [1, 2, 3, -4]
+Output: 0
+
+
+from collections import defaultdict
 
 def largestInt(A):
     mp=set()
