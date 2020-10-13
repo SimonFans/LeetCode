e1f51fe39d2406008ce582ffb54477793ca90e01
Simon Zhao <Ximengzhao1220@gmail.com>
2020-10-12 22:47:22 -0700

    Create L285_Inorder Successor in BST.py

---
+++ Tree/L285_Inorder Successor in BST.py
@@ -0,0 +1,43 @@
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+The successor of a node p is the node with the smallest key greater than p.val.
+
+Example 1:
+
+Input: root = [2,1,3], p = 1
+Output: 2
+Explanation: 1's in-order successor node is 2. Note that both p and the return value is of TreeNode type.
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], p = 6
+Output: null
+Explanation: There is no in-order successor of the current node, so the answer is null.
+
+
+Solution:
+Step1: 利用inorder遍历特性获得升序list
+Step2: 两个条件进行判断并且返回对应的值
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+        l=self.inorderTraversal(root)
+        if p.val == l[len(l)-1]:
+            return None
+        else:
+            return TreeNode(l[l.index(p.val)+1])
+    
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.val)
+            res = res + self.inorderTraversal(root.right)
+        return res
+        
