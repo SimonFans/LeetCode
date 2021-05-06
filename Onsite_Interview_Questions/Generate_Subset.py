345d4f8782eafddbd3d339d02b3a5bbdaec4467c
Simon Zhao <Ximengzhao1220@gmail.com>
2021-05-05 18:08:29 -0700

    Update Generate_Subset.py

--- Onsite_Interview_Questions/Generate_Subset.py
+++ Onsite_Interview_Questions/Generate_Subset.py
@@ -3,7 +3,6 @@ Result:  [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
 
 class solution(object):
     def generate_subset(self,nums):
-        
         def dfs(nums,index,res,path):
             res.append(path)
             for i in range(index,len(nums)):
