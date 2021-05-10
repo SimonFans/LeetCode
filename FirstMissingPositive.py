4fe546bf3fb2190aaca3cc726397a9b41dc25081
Simon Zhao <Ximengzhao1220@gmail.com>
2021-05-09 19:32:47 -0700

    Update FirstMissingPositive.py

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -14,33 +14,18 @@ Test cases:
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        if not nums:
-            return 1
-
-        length = len(nums)
-        i = 0
-        while i < length:
-            if nums[i] <= 0:
-                i += 1
-            else:
-                if nums[i] <= length:
-                    if nums[i] == i+1 or nums[nums[i]-1] == nums[i]:
-                        # no need to swap
-                        i += 1
-                        continue
-                    tmp = nums[nums[i]-1]
-                    nums[nums[i]-1] = nums[i]
-                    nums[i] = tmp
-                else:
-                    i += 1
-
-        i = 0
-        while i < length:
-            if nums[i] != i + 1:
-                return i + 1
-            i += 1
-
-        return length + 1  # no missing until length
+        # 遍历整个list
+        for i in range(len(nums)):
+            # 第一个条件用于排除负数和大于当前长度的特例，引用当前值的下标值进行判断
+            # 第二个条件用于判断是否当前值应该在的位置是否为当前值，如果是则不交换，否则交换。
+            while 0 <= nums[i]-1 < len(nums) and nums[nums[i]-1] != nums[i]:
+                tmp = nums[i]-1
+                nums[i], nums[tmp] = nums[tmp], nums[i]
+        #再次遍历整个list，判断对应位置是否为应当的值，特例是如果没有missing，比如【1，2】则应返回长度+1
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+        return len(nums)+1
     
     
 A=[3,4,-1,1]
