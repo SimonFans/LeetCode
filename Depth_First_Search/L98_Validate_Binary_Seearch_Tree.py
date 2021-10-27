004a98ffe8695555e51daf6c11513d9c8cdd0a87
Simon Zhao <Ximengzhao1220@gmail.com>
2021-10-26 20:40:11 -0700

    Create L98_Validate_Binary_Seearch_Tree.py

---
+++ Depth_First_Search/L98_Validate_Binary_Seearch_Tree.py
@@ -0,0 +1,46 @@
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Input: root = [2,1,3]
+Output: true
+  
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+  
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return
+            else:
+                dfs(root.left)
+                node_val.append(root.val)
+                dfs(root.right)
+                
+        if not root:
+            return True
+        node_val = []
+        dfs(root)
+        for i in range(len(node_val)-1):
+            if node_val[i] >= node_val[i+1]:
+                return False
+        return True
+      
