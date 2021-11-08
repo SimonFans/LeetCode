bc6235eb011bb60cc06fc8841fe06d0e60b7fe62
Simon Zhao <Ximengzhao1220@gmail.com>
2021-11-08 10:27:50 -0800

    Update L15_3Sum.py

--- Two Pointer/L15_3Sum.py
+++ Two Pointer/L15_3Sum.py
@@ -23,16 +23,19 @@ class Solution:
         nums.sort()
         res = set()
         for i in range(len(nums)):
-            j, k = i+1, len(nums) - 1
-            while j < k:
-                if nums[i] + nums[j] + nums[k] == 0:
-                    res.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
-                elif nums[i] + nums[j] + nums[k] < 0:
-                    j += 1
-                else:
-                    k -= 1
+            if nums[i] > 0: break
+            if i == 0 or nums[i-1] != nums[i]:
+                j, k = i+1, len(nums) - 1
+                while j < k:
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        res.add((nums[i], nums[j], nums[k]))
+                        j += 1
+                        k -= 1
+                    elif nums[i] + nums[j] + nums[k] < 0:
+                        j += 1
+                    else:
+                        k -= 1
+        return list(res)
         return list(res)
                 
                     
