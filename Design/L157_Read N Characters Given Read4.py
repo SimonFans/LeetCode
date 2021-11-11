cb32cb140d2588e09d7e658fc3b77d71b79b4579
Simon Zhao <Ximengzhao1220@gmail.com>
2021-11-10 16:52:06 -0800

    Update L157_Read N Characters Given Read4.py

--- Design/L157_Read N Characters Given Read4.py
+++ Design/L157_Read N Characters Given Read4.py
@@ -1,3 +1,18 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
 class Solution:
     def read(self, buf, n):
         """
@@ -7,13 +22,16 @@ class Solution:
         
         file -> buf4 -> buf
         file -> buf
-        read4() => everytime return 4 
-        read4(buf4) => save 4 chars into buf ('s','s','s','s') or ('d','','','')
         """
         
         copied_chars = 0
         read_chars = 4
         buf4 = [''] * 4
+        
+        '''
+        Input: file = "abc", n = 4
+        Output: 3
+        '''
         
         while copied_chars < n and read_chars == 4:
             # read chars into buf4 and return how many chars you read
