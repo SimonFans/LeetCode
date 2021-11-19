a4ed5b4c1f9c75c84682c89c06b5d32646a9831c
Simon Zhao <Ximengzhao1220@gmail.com>
2021-11-18 22:42:24 -0800

    Update L33_Search_In_Rotated_Sorted_Array.py

--- BinarySearch/L33_Search_In_Rotated_Sorted_Array.py
+++ BinarySearch/L33_Search_In_Rotated_Sorted_Array.py
@@ -40,3 +40,37 @@ class Solution:
                 else:
                     r=mid-1
         return -1
+    
+    
+    Solution 2:
+        class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(nums, start, end, target):
+            while start <= end:
+                mid = start + (end - start) // 2
+                if nums[mid] > target:
+                    end = mid - 1
+                elif nums[mid] < target:
+                    start = mid + 1
+                else:
+                    return mid
+            return -1      
+        
+        
+        front_right = 0
+        end_left = 0
+        end_right = len(nums) - 1 
+        ans = -1
+        
+        for i in range(end_right,0,-1):
+            if nums[i-1] > nums[i]:
+                front_right = i - 1
+                end_left = i
+                break
+
+        if target >= nums[0] and target <= nums[front_right]:
+            ans = binary_search(nums, 0, front_right, target)
+        elif target >= nums[end_left] and target <= nums[end_right]:
+            ans = binary_search(nums, end_left, end_right, target)
+        return -1 if ans == -1 else ans
+    
