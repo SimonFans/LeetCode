e5d0973bdc4531a3643e58f0f9b07abb7b8065de
Simon Zhao <Ximengzhao1220@gmail.com>
2021-11-19 10:41:20 -0800

    Update L29_Divide_Two_integers.py

--- BinarySearch/L29_Divide_Two_integers.py
+++ BinarySearch/L29_Divide_Two_integers.py
@@ -19,28 +19,20 @@ The divisor will never be 0.
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
 
 
-class Solution(object):
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        if dividend == -2**31 and divisor == -1:   # overflow case
-            return 2**31 - 1
-        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
-            if abs(dividend) < abs(divisor):
-                return 0
-        sum = 0; count = 0; res = 0
-        a = abs(dividend); b = abs(divisor)
-        while a >= b:
-            sum = b
-            count = 1
-            while sum + sum <= a:
-                sum += sum
-                count += count
-            a -= sum
-            res += count
-        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
-            res = 0 - res
-        return res
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        positive  = (dividend < 0) == (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        res = 0
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                res += i
+                #尝试乘以2去减,如果可以减，直接加倍数的量
+                i*=2
+                temp*=2
+        if not positive:
+            res = -res
+         
+        return min(max(-2**31, res), 2**31-1)
