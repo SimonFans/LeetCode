f1623dc2aa781f95e7c820d04390ec50db2d41b5
Simon Zhao <Ximengzhao1220@gmail.com>
2021-12-04 12:10:10 -0800

    Update L101_Symmetric Tree.py

--- Breadth-First-Search/L101_Symmetric Tree.py
+++ Breadth-First-Search/L101_Symmetric Tree.py
@@ -16,48 +16,51 @@ But the following [1,2,2,null,3,null,3] is not:
 Note:
 Bonus points if you could solve it both recursively and iteratively.
 
+
+# Recursive:    
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-####  BFS iterative method:
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSymmetric(self, root: TreeNode) -> bool:
-        if root == None:
-            return True
-        queue=[]
-        queue.append(root.left)
-        queue.append(root.right)
-        while queue:
-            t1=queue.pop(0)
-            t2=queue.pop(0)
-            if t1==None and t2==None:
-                continue
-            if t1==None or t2==None:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # (1) Their two roots have the same value.
+        # (2) The reflection in the mirror has the same head, but the reflection's right arm corresponds to the actual person's left arm, and vice versa.
+        def isMirror(t1, t2):
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
                 return False
-            if t1.val!=t2.val:
+            return t1.val == t2.val and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
+        
+        return isMirror(root, root)
+
+# Iterativve:
+from collections import deque
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # iteration:
+        # (1) two roots add into the queue
+        # (2) Each two consecutive nodes in the queue should be equal
+        
+        queue = deque()
+        queue.append(root)
+        queue.append(root)
+        while queue:
+            t1 = queue.popleft()
+            t2 = queue.popleft()
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None:
+                return False
+            if t1.val != t2.val:
                 return False
             queue.append(t1.left)
             queue.append(t2.right)
             queue.append(t1.right)
             queue.append(t2.left)
         return True
-        
-        
-### Recursive way     
-class Solution:
-    def isSymmetric(self, root: TreeNode) -> bool:
-        if root:
-            return self.helper(root.left,root.right)
-        return True
-        
-    def helper(self,p,q):
-        if p==None and q==None:
-            return True
-        if p and q and p.val==q.val:
-            return self.helper(p.right,q.left) and self.helper(p.left,q.right)
-        return False
+    
+ 
