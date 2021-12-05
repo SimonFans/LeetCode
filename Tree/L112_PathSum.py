f124472e006a3d8f8c15b503dff8073dca855159
Simon Zhao <Ximengzhao1220@gmail.com>
2021-12-04 17:35:53 -0800

    Update L112_PathSum.py

--- Tree/L112_PathSum.py
+++ Tree/L112_PathSum.py
@@ -17,20 +17,36 @@ Given the below binary tree and sum = 22,
 
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def hasPathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: bool
-        """
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # recursive
         if not root:
             return False
-        if root.left == None and root.right == None and root.val==sum:
-            return True
-        return self.hasPathSum(root.left,sum-root.val) or self.hasPathSum(root.right,sum-root.val)
+        
+        targetSum -= root.val
+        # if current node is a leaf node, then check if current sum == 0? 
+        if not root.left and not root.right:
+            return targetSum == 0
+        
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+    
+        # iterative
+        if not root:
+            return False
+        
+        q = [(root, targetSum - root.val)]
+        
+        while q:
+            current_node, remain = q.pop(0)
+            if not current_node.left and not current_node.right and remain == 0:
+                return True
+            if current_node.left:
+                q.append((current_node.left, remain - current_node.left.val))
+            if current_node.right:
+                q.append((current_node.right, remain - current_node.right.val))
+        return False
+
