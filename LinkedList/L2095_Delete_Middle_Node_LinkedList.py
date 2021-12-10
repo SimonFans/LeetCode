ca779eee03b96c7bd57811d67cd49956f0eb99cc
Simon Zhao <Ximengzhao1220@gmail.com>
2021-12-10 09:50:10 -0800

    Create L2095_Delete_Middle_Node_LinkedList.py

---
+++ LinkedList/L2095_Delete_Middle_Node_LinkedList.py
@@ -0,0 +1,46 @@
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+
+
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast, c = head, head, head
+        
+        if slow.next is None:
+            return None
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        while c.next != slow:
+            c = c.next
+            
+        c.next = slow.next
+        
+        return head
+      
+      
