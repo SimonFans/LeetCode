9128306c61764817e5462e50de878424dcb866c9
Simon Zhao <Ximengzhao1220@gmail.com>
2021-12-11 16:38:28 -0800

    Update Generate_Subset.py

--- Onsite_Interview_Questions/Generate_Subset.py
+++ Onsite_Interview_Questions/Generate_Subset.py
@@ -1,15 +1,26 @@
 Given [1,2,3]
 Result:  [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
 
-class solution(object):
-    def generate_subset(self,nums):
-        def dfs(nums,index,res,path):
-            res.append(path)
-            for i in range(index,len(nums)):
-                dfs(nums,i+1,res,path+[nums[i]])
-        res=[]
-        dfs(nums,0,res,[])
-                
-        return res
-
-print(solution().generate_subset([1,2,3]))
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        Backtracking is an algorithm for finding all solutions by exploring all potential candidates. If the solution candidate turns to be not a solution (or at least not the last one), backtracking algorithm discards it by making some changes on the previous step, i.e. backtracks and then try again.
+        '''
+        def backtrack(first=0, curr=[]):
+            # if the current combination is done  which means length of curr == k 
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
+            for i in range(first, n):
+                curr.append(nums[i])
+                backtrack(i+1, curr)
+                print(curr)
+                curr.pop()
+                print(curr)
+        
+        ans = []
+        n = len(nums)
+        # why n+1 because you want to cover empty case, k => all subsets of length k
+        for k in range(n+1):
+            backtrack()
+        return ans
