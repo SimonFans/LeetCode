30b9b0e038336725495926180e9879fe45ce1905
Simon Zhao <Ximengzhao1220@gmail.com>
2022-01-06 18:18:37 -0800

    Create L2090_K_Radius_Subarray_Averages.py

---
+++ Sliding_Window/L2090_K_Radius_Subarray_Averages.py
@@ -0,0 +1,54 @@
+'''
+You are given a 0-indexed array nums of n integers, and an integer k.
+
+The k-radius average for a subarray of nums centered at some index i with the radius k is the average of all elements in nums between the indices i - k and i + k (inclusive). If there are less than k elements before or after the index i, then the k-radius average is -1.
+
+Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+
+The average of x elements is the sum of the x elements divided by x, using integer division. The integer division truncates toward zero, which means losing its fractional part.
+
+For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4 = 11 / 4 = 2.75, which truncates to 2.
+
+'''
+
+
+Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
+Output: [-1,-1,-1,5,4,4,-1,-1,-1]
+Explanation:
+- avg[0], avg[1], and avg[2] are -1 because there are less than k elements before each index.
+- The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 + 1 + 8 + 5 = 37.
+  Using integer division, avg[3] = 37 / 7 = 5.
+- For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7 = 4.
+- For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7 = 4.
+- avg[6], avg[7], and avg[8] are -1 because there are less than k elements after each index.
+
+
+Input: nums = [100000], k = 0
+Output: [100000]
+Explanation:
+- The sum of the subarray centered at index 0 with radius 0 is: 100000.
+  avg[0] = 100000 / 1 = 100000.
+  
+  
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        '''
+        [7,4,3,9,1,8,5,2,6]
+        考点： 如可确定window length
+        window length = 2 * k + 1 (2*半径 + 1 自己)
+        '''
+        ans = [-1] * len(nums)
+        window_sum = 0
+        left = 0
+        # window length depends on radius k
+        window_len  = 2*k + 1
+        
+        for right in range(len(nums)):
+            window_sum += nums[right]
+            if right - left + 1 >= window_len:
+                ans[left+k] = window_sum // window_len
+                window_sum -= nums[left]
+                left += 1
+        return ans
+      
+      
