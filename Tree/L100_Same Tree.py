03dac92c3df96a6cd7a66f5f7a4a3d32675eeb5c
Simon Zhao <Ximengzhao1220@gmail.com>
2022-01-12 18:29:13 -0800

    Update L100_Same Tree.py

--- Tree/L100_Same Tree.py
+++ Tree/L100_Same Tree.py
@@ -33,22 +33,21 @@ Output: false
 
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+                
         if p is None and q is None:
             return True
         
-        if p is not None and q is not None:
-            return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        if p is None or q is None:
+            return False
         
-        # if structure differ, p is None, q is not None
-        return False
+        if p is not None and q is not None:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
         
         
