c5a374b7565bb4bc4cd56634e7fee20ac85e81e5
Simon Zhao <Ximengzhao1220@gmail.com>
2022-01-24 16:12:58 -0800

    Update L605_Can_Place_Flowers.py

--- Array/L605_Can_Place_Flowers.py
+++ Array/L605_Can_Place_Flowers.py
@@ -14,10 +14,19 @@ Output: false
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         # find out how many valid positions that we can place flowers
-        i, count = 0, 0 
-        while i < len(flowerbed):
-            if flowerbed[i] ==0 and (i ==0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
-                flowerbed[i] = 1
-                count += 1
-            i+=1
-        return count >= n
+        
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f) - 1):
+            if f[i] == 0 and f[i+1] == 0 and f[i-1] == 0:
+                f[i] = 1
+                n -= 1
+        return n <= 0
+      
+      
+#         i, count = 0, 0 
+#         while i < len(flowerbed):
+#             if flowerbed[i] ==0 and (i ==0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+#                 flowerbed[i] = 1
+#                 count += 1
+#             i+=1
+#         return count >= n
