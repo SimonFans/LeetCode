436ebac305415679be6e48dd2830d123bc065629
Simon Zhao <Ximengzhao1220@gmail.com>
2022-01-24 22:20:19 -0800

    Update L678_Valid Parenthesis String.py

--- String/L678_Valid Parenthesis String.py
+++ String/L678_Valid Parenthesis String.py
@@ -15,29 +15,25 @@ Example 3:
 Input: "(*))"
 Output: True
 
-# 正向反向各扫一遍
 
 class Solution:
-    def checkValidString(self, s: 'str') -> 'bool':
-        # left: 遇左括号和星号都加1，如果Left为0，则either没星号或者星号当作左括号与右边匹配
-        left,right=0,0
-        size=len(s)
-        for i in range(size):
-            if s[i]=='(' or s[i]=='*':
-                left+=1
+    def checkValidString(self, s: str) -> bool:
+        '''
+        li: smallest possible number of open left brackets
+        hi: largest possible number of open left brackets
+        '''
+        li, hi = 0, 0
+        for char in s:
+            if char == '(':
+                li += 1
             else:
-                left-=1
-            if left<0:
-                return False
-        if left==0:
-            return True
-        # right: 遇右括号和星号都加1，如果right为0，* match右括号, 如果right大于0，星号可为空
-        for i in range(size-1,-1,-1):
-            if s[i]==')' or s[i]=='*':
-                right+=1
+                li -= 1
+            if char != ')':
+                hi += 1
             else:
-                right-=1
-            if right<0:
-                return False
-        return True
+                hi -= 1
+            if hi < 0:
+                break
+            li = max(li, 0)
+        return li == 0
         
