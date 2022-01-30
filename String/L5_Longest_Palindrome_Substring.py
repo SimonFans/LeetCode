2763be28d404dddd157135098fc82c2a599bcd44
Simon Zhao <Ximengzhao1220@gmail.com>
2022-01-29 18:30:21 -0800

    Update L5_Longest_Palindrome_Substring.py

--- String/L5_Longest_Palindrome_Substring.py
+++ String/L5_Longest_Palindrome_Substring.py
@@ -12,7 +12,18 @@ Output: "bb"
     
     
 # 中心扩散法，分奇偶情况,每次传基数长度之后偶数长度
-# O(n) O(1)
+# Time: O(n^2): Since expanding a palindrome around its center could take O(n) time, the overall complexity is O(n^2).
+# Space: O(1) 
+
+
+'''
+Thoughts:
+a palindrome mirrors around its center. Therefore, a palindrome can be expanded from its center.
+If n is the length of the string, then there's only 2n - 1 such centers.
+example:
+s = 'abc'
+so the center will be a, b, c, ab, and bc which is 2n-1 = 5
+'''
     
 class Solution:
     def longestPalindrome(self, s: str) -> str:
