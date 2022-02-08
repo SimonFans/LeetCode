9374dd328cc555884be3338a741d733b03132514
Simon Zhao <Ximengzhao1220@gmail.com>
2022-02-07 18:26:40 -0800

    Update L43_Multipy_Strings.py

--- String/L43_Multipy_Strings.py
+++ String/L43_Multipy_Strings.py
@@ -15,33 +15,37 @@ Both num1 and num2 contain only digits 0-9.
 Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
+
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1=="0" or num2=="0":
+        '''
+        num1 = '12', num2 = '11'
+        '''
+        
+        # Any of two input strings is '0' then return '0' 
+        if num1 == '0' or num2 == '0':
             return '0'
         
-        # 取反，这样最右边的相乘数据在最左边，剩下的后面都是0，例：【72,109,119,0,0】
-        num1=num1[::-1]
-        num2=num2[::-1]
-
-        arr=[0]*(len(num1)+len(num2))
+        # initialize result list with length(nums1) + length(nums2)
+        res = [0] * (len(num1) + len(num2))
         
-        for i in range(len(num1)):
-            for j in range(len(num2)):
-                arr[i+j]+=int(num1[i])*int(num2[j])
-        res=[]
-        carry=0
-
-        for k in range(len(arr)):
-            number=(arr[k]+carry)%10
-            res.append(str(number))
-            carry=(arr[k]+carry)//10
+        # reverse the string 
+        first_number = num1[::-1]
+        second_number = num2[::-1]
         
-        res=res[::-1]
+        for index2, num_2 in enumerate(second_number):
+            for index1, num_1 in enumerate(first_number):
+                pos = index1 + index2
+                # load carry flag value
+                carry = res[pos]
+                # set the position value
+                res[pos] = (int(num_1)*int(num_2) + carry) % 10
+                # add to the next position in the result list
+                res[pos+1] += (int(num_1)*int(num_2) + carry) // 10
         
-        # consider case ['0',0','6'], need to remove leading zeros
-        
-        while res[0]=='0' and len(res)>1:
-            del res[0]
-            
-        return ''.join(res)
+        # if num1=12, num2 = 6 then actual return should length = 2, so remove the last pos 
+        if res[-1] == 0:
+            res.pop()
+        return ''.join([str(num) for num in res[::-1]])
+    
+    
