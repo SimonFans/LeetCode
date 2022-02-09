423dc4174b408dd09f39b8571649c8542a77c4b0
Simon Zhao <Ximengzhao1220@gmail.com>
2022-02-09 12:12:50 -0800

    Update fb_L253_minMeetingRoomsII.py

--- Onsite_Interview_Questions/fb_L253_minMeetingRoomsII.py
+++ Onsite_Interview_Questions/fb_L253_minMeetingRoomsII.py
@@ -5,56 +5,27 @@ For example,
 Given [[0, 30],[5, 10],[15, 20]],
 return 2.
 
-# Method 1: heapq
 
 from heapq import heappush, heappop
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        
         if not intervals:
             return 0
+        heap = []
         
-        # 按开始时间排序
-        intervals.sort(key=lambda x:x[0])
+        #sort the list based on start time
+        intervals.sort(key = lambda x: x[0])
         
-        # 建立堆结构
-        room=[]
+        # heap push the first meeting end time
+        heapq.heappush(heap, intervals[0][1])
         
-        # 放入第一个会议结束时间
-        heappush(room,intervals[0][1])
-        
-        for i in intervals[1:]:
-            # 如果之前会议最早的结束时间<当前会议的开始时间，就将当前会议结束时间更新为最新结束时间
-            if room[0]<=i[0]:
-                heappop(room)
-            # （1）最近的会议都在开，只能开新房间 （2）找到并延长之前的会议结束时间
-            heappush(room,i[1])
-        
-        # 最后算有多少个结束时间就是多少个房间
-        return len(room)
-
-# Method 2:
-
-class solution:
-    def minMeetingRoomsII(self,test):
-
-        starts_sort=sorted(test, key=lambda x: x[0])   # [[0,30],[5,10],[15,20]]
-        ends_sort=sorted(test,key=lambda x: x[1])      # [[5,10],[15,20],[0,30]]
-        starts=[]
-        ends=[]
-        for i in range(len(test)):                     # run 0, 1, 2
-            starts.append(starts_sort[i][0])           #[0,5,15]
-            ends.append(ends_sort[i][1])               #[10,20,30]
-        res=0
-        end=0
-        for i in range(len(test)):
-            if starts[i]<ends[end]:
-                res+=1
-            else:
-                end+=1
-        return res
-
-
-test=[[0,30],[5,10],[15,20]]
-print(solution().minMeetingRoomsII(test))
+        # if next start time >= the minimum end time, then it means there's a meeting completes
+        # we can pop the minimum end time
+        for interval in intervals[1:]:
+            if heap[0] <= interval[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, interval[1])
+        return len(heap)
+    
+    
