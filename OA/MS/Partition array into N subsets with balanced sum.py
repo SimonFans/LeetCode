aaf7a86e9526d1364d9e0130b948f39770ebb444
Simon Zhao <Ximengzhao1220@gmail.com>
2022-04-25 13:00:29 -0700

    Update Partition array into N subsets with balanced sum.py

--- OA/MS/Partition array into N subsets with balanced sum.py
+++ OA/MS/Partition array into N subsets with balanced sum.py
@@ -53,5 +53,43 @@ class Solution():
 s = Solution()
 
 assert s.solution([4, 3, 2, 3, 5, 2, 1],4) == True
-
+        
 print("pass all test case")
+        
+class Solution:
+    def canPartitionKSubsets(self, arr: List[int], k: int) -> bool:
+        def backtrack(count: int, curr_sum: int) -> bool:
+        # We made k - 1 subsets with target sum and the last subset will also have target sum.
+            if count == k - 1:
+                return True
+            # Current subset-sum exceeds target sum, no need to proceed further.
+            if curr_sum > target_sum:
+                return False
+            # When current subset sum reaches target sum then one subset is made.
+            # Increment count and reset current subset sum to 0.
+            if curr_sum == target_sum:
+                return backtrack(count + 1, 0)
+
+            # Try not picked elements to make some combinations.
+            for j in range(n):
+                if not taken[j]:
+            # Include this element in current subset.
+                    taken[j] = True
+            # If using current jth element in this subset leads to make all valid subsets.
+                    if backtrack(count, curr_sum + arr[j]):
+                        return True
+            # Backtrack step.
+                    taken[j] = False
+            # We were not able to make a valid combination after picking 
+            # each element from the array, hence we can't make k subsets.
+            return False
+        total_array_sum = sum(arr)
+        n = len(arr)
+        # If the total sum is not divisible by k, we can't make subsets.
+        if total_array_sum % k != 0:
+            return False
+        target_sum = total_array_sum // k
+        taken = [False] * n
+        return backtrack(0, 0)
+        
+        
