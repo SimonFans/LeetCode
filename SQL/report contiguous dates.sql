eefdfaa848cd964d39715a62974406193af85952
Simon Zhao <Ximengzhao1220@gmail.com>
2024-08-21 09:29:02 -0700

    Create report contiguous dates.sql

---
+++ SQL/report contiguous dates.sql
@@ -0,0 +1,58 @@
+Input: 
+Failed table:
++-------------------+
+| fail_date         |
++-------------------+
+| 2018-12-28        |
+| 2018-12-29        |
+| 2019-01-04        |
+| 2019-01-05        |
++-------------------+
+Succeeded table:
++-------------------+
+| success_date      |
++-------------------+
+| 2018-12-30        |
+| 2018-12-31        |
+| 2019-01-01        |
+| 2019-01-02        |
+| 2019-01-03        |
+| 2019-01-06        |
++-------------------+
+Output: 
++--------------+--------------+--------------+
+| period_state | start_date   | end_date     |
++--------------+--------------+--------------+
+| succeeded    | 2019-01-01   | 2019-01-03   |
+| failed       | 2019-01-04   | 2019-01-05   |
+| succeeded    | 2019-01-06   | 2019-01-06   |
++--------------+--------------+--------------+
+Explanation: 
+The report ignored the system state in 2018 as we care about the system in the period 2019-01-01 to 2019-12-31.
+From 2019-01-01 to 2019-01-03 all tasks succeeded and the system state was "succeeded".
+From 2019-01-04 to 2019-01-05 all tasks failed and the system state was "failed".
+From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succeeded".
+
+
+
+
+
+SELECT stats AS period_state, MIN(day) AS start_date, MAX(day) AS end_date
+FROM (
+    SELECT 
+        day, 
+        RANK() OVER (ORDER BY day) AS overall_ranking, 
+        stats, 
+        rk, 
+        (RANK() OVER (ORDER BY day) - rk) AS inv
+    FROM (
+        SELECT fail_date AS day, 'failed' AS stats, RANK() OVER (ORDER BY fail_date) AS rk
+        FROM Failed
+        WHERE fail_date BETWEEN '2019-01-01' AND '2019-12-31'
+        UNION 
+        SELECT success_date AS day, 'succeeded' AS stats, RANK() OVER (ORDER BY success_date) AS rk
+        FROM Succeeded
+        WHERE success_date BETWEEN '2019-01-01' AND '2019-12-31') t
+    ) c
+GROUP BY inv, stats
+ORDER BY start_date
