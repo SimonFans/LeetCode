100eaf58f9ddd96cf1bcb1b52a283a37e137a3aa
Simon Zhao <Ximengzhao1220@gmail.com>
2024-08-21 10:59:29 -0700

    Update find cumulative salary of an employee.sql

--- SQL/find cumulative salary of an employee.sql
+++ SQL/find cumulative salary of an employee.sql
@@ -1,3 +1,49 @@
+Write a solution to calculate the cumulative salary summary for every employee in a single unified table.
+
+The cumulative salary summary for an employee can be calculated as follows:
+
+For each month that the employee worked, sum up the salaries in that month and the previous two months. This is their 3-month sum for that month. If an employee did not work for the company in previous months, their effective salary for those months is 0.
+Do not include the 3-month sum for the most recent month that the employee worked for in the summary.
+Do not include the 3-month sum for any month the employee did not work.
+Return the result table ordered by id in ascending order. In case of a tie, order it by month in descending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+
+| id | month | salary |
++----+-------+--------+
+| 1  | 1     | 20     |
+| 2  | 1     | 20     |
+| 1  | 2     | 30     |
+| 2  | 2     | 30     |
+| 3  | 2     | 40     |
+| 1  | 3     | 40     |
+| 3  | 3     | 60     |
+| 1  | 4     | 60     |
+| 3  | 4     | 70     |
+| 1  | 7     | 90     |
+| 1  | 8     | 90     |
++----+-------+--------+
+Output: 
++----+-------+--------+
+| id | month | Salary |
++----+-------+--------+
+| 1  | 7     | 90     |
+| 1  | 4     | 130    |
+| 1  | 3     | 90     |
+| 1  | 2     | 50     |
+| 1  | 1     | 20     |
+| 2  | 1     | 20     |
+| 3  | 3     | 100    |
+| 3  | 2     | 40     |
++----+-------+--------+
+
 with cte as (
 select 
   e1.id,
