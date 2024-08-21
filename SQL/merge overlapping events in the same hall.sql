ad48ac9202c402b7ba212d27776146994c52cd6a
Simon Zhao <Ximengzhao1220@gmail.com>
2024-08-21 10:28:28 -0700

    Update merge overlapping events in the same hall.sql

--- SQL/merge overlapping events in the same hall.sql
+++ SQL/merge overlapping events in the same hall.sql
@@ -1,3 +1,43 @@
+Write a solution to merge all the overlapping events that are held in the same hall. Two events overlap if they have at least one day in common.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+Example 1:
+
+Input: 
+HallEvents table:
++---------+------------+------------+
+| hall_id | start_day  | end_day    |
++---------+------------+------------+
+| 1       | 2023-01-13 | 2023-01-14 |
+| 1       | 2023-01-14 | 2023-01-17 |
+| 1       | 2023-01-18 | 2023-01-25 |
+| 2       | 2022-12-09 | 2022-12-23 |
+| 2       | 2022-12-13 | 2022-12-17 |
+| 3       | 2022-12-01 | 2023-01-30 |
++---------+------------+------------+
+Output: 
++---------+------------+------------+
+| hall_id | start_day  | end_day    |
++---------+------------+------------+
+| 1       | 2023-01-13 | 2023-01-17 |
+| 1       | 2023-01-18 | 2023-01-25 |
+| 2       | 2022-12-09 | 2022-12-23 |
+| 3       | 2022-12-01 | 2023-01-30 |
++---------+------------+------------+
+Explanation: There are three halls.
+Hall 1:
+- The two events ["2023-01-13", "2023-01-14"] and ["2023-01-14", "2023-01-17"] overlap. We merge them in one event ["2023-01-13", "2023-01-17"].
+- The event ["2023-01-18", "2023-01-25"] does not overlap with any other event, so we leave it as it is.
+Hall 2:
+- The two events ["2022-12-09", "2022-12-23"] and ["2022-12-13", "2022-12-17"] overlap. We merge them in one event ["2022-12-09", "2022-12-23"].
+Hall 3:
+- The hall has only one event, so we return it. Note that we only consider the events of each hall separately.
+
+
 # Write your MySQL query statement below
 
 with aa as (
